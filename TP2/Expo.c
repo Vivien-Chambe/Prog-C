9ebf2f0a44da4413b6a4a3a6e6005a6d00744b02
neiviv-dev <vivien.chambe@live.com>
2022-09-14 15:03:42 +0200

    Question 3

--- TP2/Expo.c
+++ TP2/Expo.c
@@ -76,6 +76,38 @@ void testslowexp(int n)
 }
 }
 
+//Question n°3 :
+
+// In: 0 <= a < 536870909
+// In: 0 <= n < 2**64
+// Out: 0 <= x < 536870909 t.q. x ~ a^n [536870909]
+uint64_t fast_exp_rec(uint64_t a, uint64_t n)
+{
+    uint64_t r=1;
+    
+    if (n==0){ return r;}
+    r = (fast_exp_rec(a,n/2));
+    if (n % 2 == 0){ return mul293(r,r); }
+    else {return mul293(mul293(r,r),a);}
+    
+    
+}
+
+void testrecexp(int n)
+{
+    assert(fast_exp_rec(2022,36) == 233688688);
+    assert(fast_exp_rec(2022,37)== 72127216);
+    assert(fast_exp_rec(200022,36)== 504243905);
+    assert(fast_exp_rec(200022,37)== 84175716);
+    assert(fast_exp_rec(2022,1e9)== 274298323);
+    
+    for(int i = 1; i<n;i++){
+    printf("10^%d :%lu \n",i, (fast_exp_rec(2022,fast_exp_rec(10,i))));
+    }
+}
+
+//temps d'exeion grandement ameliore passant a seulement 0,015s pour n = 10
+//le cout est maintenant
 
 int main()
 {
@@ -85,9 +117,10 @@ int main()
     //printf("add109 :%lu \n",add109(456789,5432109));
     //printf("mul109 :%lu \n",mul109(456789,5432109));
 
-    testslowexp(10);    
+    //testslowexp(10);    
     //le cout de la fonction est exponentiel si on affiche les resultats pour 4 test nous avons 0,013sec 
     // et pour 9 nous avons 15 sec
-
+    printf("%d ^ %f = %lu \n",2022,1e19,fast_exp_rec(2022,1e9));
+    testrecexp(10);
     return 0;
 }
